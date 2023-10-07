abde64a9d4626df54954ee32ddc1aa4f2ae9690f
jlearns9 <142127358+jlearns9@users.noreply.github.com>
2023-10-07 00:24:36 -0500

    uPdate

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ import menuArray from "./data.js";
 const test = document.getElementById('test')
 const consoleTest = document.getElementById('console-test')
 
-function html() {
-    test.innerHTML = 'this is a tdest';
+function renderMenu() {
+    test.innerHTML = menuArray.map(menuItem => `
+    <div>
+        <div>${menuItem.emoji}</div>
+        <h3>${menuItem.name}</h3>
+        <p>${menuItem.ingredients}</p>
+        <h4>${menuItem.name}</h4>
+        <button>+</button>
+    </div>`
+    )
 }
 
-html()
+renderMenu()
 
 
 
